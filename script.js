da59ab5a8a0c94c64c545ab9a2e556d903b7d06a
Bruno Sinhorelli <b.sinhorelli@gmail.com>
2026-09-23 22:13:39 -0300

    segundo exercicio resolvido

--- script.js
+++ script.js
@@ -13,24 +13,45 @@
 //     alert(`Nome digitado: ${nomeUsuario}`)
 // };
 
-var num = Number(prompt('Digite um número de 1 a 5: '));
+// var num = Number(prompt('Digite um número de 1 a 5: '));
 
-if (num == 1) {
-    alert('Um');
+// if (num == 1) {
+//     alert('Um');
+// }
+// else if ( num == 2) {
+//     alert('Dois')
+// }
+// else if ( num == 3) {
+//     alert('Três')
+// }
+// else if ( num == 4) {
+//     alert('Quatro')
+// }
+// else if ( num == 5) {
+//     alert('Cinco')
+// }
+// else {
+//     alert('Digite um número que seja de 1 a 5!')
+// };
+
+var num = Number(prompt('Digite um número de 1 a 50: '));
+
+if (num <= 10) {
+    console.log('O número digitado é:', num);
+    alert('O número digitado está entre 1 e 10: ', num);
 }
-else if ( num == 2) {
-    alert('Dois')
+else if (num <= 30) {
+    console.log('O número digitado é:', num);
+    alert('O número digitado está entre 21 e 30: ', num)
 }
-else if ( num == 3) {
-    alert('Três')
+else if (num <= 40) {
+    console.log('O número digitado é:', num);
+    alert('O número digitado está entre 31 e 40: ', num)
 }
-else if ( num == 4) {
-    alert('Quatro')
-}
-else if ( num == 5) {
-    alert('Cinco')
+else if (num <= 50) {
+    console.log('O número digitado é:', num);
+    alert('O número digitado está entre 41 e 50: ', num)
 }
 else {
-    alert('Digite um número que seja de 1 a 5!')
+    alert('O número digitado não está no intervalo!');
 };
-
