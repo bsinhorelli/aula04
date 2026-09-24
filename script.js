630efda0c417603806615100a8e458f58fde41d8
Bruno Sinhorelli <b.sinhorelli@gmail.com>
2026-09-23 22:35:16 -0300

    resolucao

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // var nomeUsuario = prompt('Digite seu nome: ');
 
-// if (nomeUsuario == '') {
+// if ((nomeUsuario == '') &&  (nomeUsuario == null)) {
 //     alert('O usuário não informou o nome!')
 // }
 
@@ -11,6 +11,12 @@
 
 // else {
 //     alert(`Nome digitado: ${nomeUsuario}`)
+// };
+
+// var nome = prompt('Digite um nome: ')
+
+// if ((nome != '') && ((nome == 'Ema') || (nome == 'EMA') || (nome = 'ema'))) {
+
 // };
 
 // var num = Number(prompt('Digite um número de 1 a 5: '));
@@ -34,24 +40,24 @@
 //     alert('Digite um número que seja de 1 a 5!')
 // };
 
-var num = Number(prompt('Digite um número de 1 a 50: '));
+// var num = Number(prompt('Digite um número de 1 a 50: '));
 
-if (num <= 10) {
-    console.log('O número digitado é:', num);
-    alert('O número digitado está entre 1 e 10: ', num);
-}
-else if (num <= 30) {
-    console.log('O número digitado é:', num);
-    alert('O número digitado está entre 21 e 30: ', num)
-}
-else if (num <= 40) {
-    console.log('O número digitado é:', num);
-    alert('O número digitado está entre 31 e 40: ', num)
-}
-else if (num <= 50) {
-    console.log('O número digitado é:', num);
-    alert('O número digitado está entre 41 e 50: ', num)
-}
-else {
-    alert('O número digitado não está no intervalo!');
-};
+// if (num <= 10) {
+//     console.log('O número digitado é:', num);
+//     alert('O número digitado está entre 1 e 10: ', num);
+// }
+// else if (num <= 30) {
+//     console.log('O número digitado é:', num);
+//     alert('O número digitado está entre 21 e 30: ', num)
+// }
+// else if (num <= 40) {
+//     console.log('O número digitado é:', num);
+//     alert('O número digitado está entre 31 e 40: ', num)
+// }
+// else if (num <= 50) {
+//     console.log('O número digitado é:', num);
+//     alert('O número digitado está entre 41 e 50: ', num)
+// }
+// else {
+//     alert('O número digitado não está no intervalo!');
+// };
