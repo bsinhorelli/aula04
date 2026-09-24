47759d943cfa775959fb9c1a2833ed8ae56443d1
Bruno Sinhorelli <b.sinhorelli@gmail.com>
2026-09-23 22:05:53 -0300

    exercicio 1 resolvido em aula

---
+++ script.js
@@ -0,0 +1,36 @@
+// var nomeUsuario = prompt('Digite seu nome: ');
+
+// if (nomeUsuario == '') {
+//     alert('O usuário não informou o nome!')
+// }
+
+// else if (nomeUsuario == null) {
+//     alert('O nome do usuário é nulo')
+//     nomeUsuario = prompt('Por favor, forneça o nome: ')
+// }
+
+// else {
+//     alert(`Nome digitado: ${nomeUsuario}`)
+// };
+
+var num = Number(prompt('Digite um número de 1 a 5: '));
+
+if (num == 1) {
+    alert('Um');
+}
+else if ( num == 2) {
+    alert('Dois')
+}
+else if ( num == 3) {
+    alert('Três')
+}
+else if ( num == 4) {
+    alert('Quatro')
+}
+else if ( num == 5) {
+    alert('Cinco')
+}
+else {
+    alert('Digite um número que seja de 1 a 5!')
+};
+
